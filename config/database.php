5b9115f793d4e0502848102cb94df84d218a94c5
Ulisses Mantovani <mantovaniarts@hotmail.com>
2016-06-29 22:45:20 -0300

    bug fixed

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'manto638_laravel'),
-            'username'  => env('DB_USERNAME', 'manto638_admin'),
-            'password'  => env('DB_PASSWORD', '1010666'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
