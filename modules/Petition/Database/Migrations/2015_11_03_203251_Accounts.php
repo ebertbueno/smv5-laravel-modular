8ea7371c25ae7a4f7c8cd8324643dfa83c4cd2e2
Ulisses Mantovani <mantovaniarts@hotmail.com>
2016-08-01 12:36:35 -0300

    Autentication added

--- modules/Petition/Database/Migrations/2015_11_03_203251_Accounts.php
+++ modules/Petition/Database/Migrations/2015_11_03_203251_Accounts.php
@@ -15,7 +15,6 @@ class Accounts extends Migration
         Schema::create('accounts', function (Blueprint $table) 
         {
             $table->integer('user_id')->unsigned()->index();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('avatar');
             $table->string('provider');
             $table->string('provider_id');
